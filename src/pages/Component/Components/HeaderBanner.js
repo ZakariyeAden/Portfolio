1ed0b231ca1c43edfe71acb828ca9a7c836e2582
ZakariyeAden <zakariye5417@gmail.com>
2023-02-28 13:30:27 -0600

    adding particles background

--- src/pages/Component/Components/HeaderBanner.js
+++ src/pages/Component/Components/HeaderBanner.js
@@ -2,40 +2,48 @@ import React, { useEffect } from "react";
 
 import Aos from "aos";
 import Links from '../Links'
-import Profile from '../../../assets/Images/About/Profile.png'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import ParticlesBackground from "./ParticlesBackground";
+
 import "aos/dist/aos.css";
-const HeaderBanner = () => {
+const HeaderBanner = (props) => {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
+
+  const [text] = useTypewriter({
+		words: ["a Web Developer.", "a Full-stack Developer.", "a Front-end Developer."],
+		loop: {},
+		typeSpeed: 120,
+		deleteSpeed: 120
+	});
+
   return (
     <>
+  
       <div id="Home">
-        <div className="home-section">
-        
+        <div className="home-section" >
+         
           <div className="home">
-          <span className=" ">
-            <div className="">
-           
-            </div>
-          </span>
-            <div className="img-bnr-text ">
+          <div>
+            <ParticlesBackground/>
+          </div>
+            <div className="img-bnr-text">
               <div>
-                <h5 data-aos="fade-down " className="heading">Web Developer</h5>
+                
+                <h5 data-aos="fade-down " className="heading">Hello, my name is,</h5>
                 <h1 data-aos="flip-down">Zakariye Aden</h1>
+                <h3>Im <span className="position">{text}</span></h3>
                 <h6 className="description">
-                  Passionate about building User Interface and 
-                  <br />
-                  learning new skills or technologies that are growing within web development or tech. 
-                  
-                  <br />
-                  Always motivated and great team member who can add value and grow within the team.
+                  Passionate about building User Interface, designing, and being Critirea Team Member
                 </h6>
                 <Links href="#About">
                 <button className="BtnView">About Us</button>
                 </Links>
+        
               </div>
             </div>
+          
           </div>
         </div>
       </div>
